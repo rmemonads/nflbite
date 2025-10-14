71ee48eeab6b35237c4b123e26d3ca553884bdd0
rmemonads <rmemonads3132@gmail.com>
2025-10-15 02:02:36 +0600

    Update watch.js

--- Watch/watch.js
+++ Watch/watch.js
@@ -360,7 +360,7 @@ async function loadDiscordWidget() {
             const moreLink = document.createElement('a');
             moreLink.href = inviteUrl;
             moreLink.target = '_blank';
-            moreLink.rel = 'noopener noreferrer';
+            moreLink.rel = 'noopener noreferrer nofollow';
             moreLink.textContent = 'Discord!';
             p.append('and more in our ', moreLink);
             moreLi.appendChild(p);
@@ -381,5 +381,6 @@ document.addEventListener("DOMContentLoaded", () => {
     setupSearch(); 
     loadDiscordWidget();
 });
+
 
 window.addEventListener('hashchange', initializeWatchPage);
